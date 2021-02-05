7c0da9db29865b2f3d3f8d914f030d02b8972d8e
ngoandrew <53445172+ngoandrew@users.noreply.github.com>
2021-02-05 13:30:04 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,13 @@
 // main.js
 
+function changeSrc() {
+    if (document.getElementById("radio-air-horn").checked) {
+      document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+    } else if (document.getElementById("radio-car-horn").checked) { 
+      document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+    } else if (document.getElementById("radio-party-horn").checked) { 
+      document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+    }
+}
+
 // TODO
