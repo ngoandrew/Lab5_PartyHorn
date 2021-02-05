ef59a459129d74266495310b19888ab781b639e0
ngoandrew <53445172+ngoandrew@users.noreply.github.com>
2021-02-05 13:52:26 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,7 @@
 // main.js
 
+
+// Handle radio input
 document.getElementById("radio-air-horn").addEventListener("click", changeSrc);
 document.getElementById("radio-car-horn").addEventListener("click", changeSrc);
 document.getElementById("radio-party-horn").addEventListener("click", changeSrc);
@@ -7,11 +9,20 @@ document.getElementById("radio-party-horn").addEventListener("click", changeSrc)
 function changeSrc() {
     if (document.getElementById("radio-air-horn").checked) {
       document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+      document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
     } else if (document.getElementById("radio-car-horn").checked) { 
       document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+      document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
     } else if (document.getElementById("radio-party-horn").checked) { 
       document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+      document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
     }
 }
 
-// TODO
+// Handle honk button
+document.getElementById("honk-btn").addEventListener("click", honk);
+
+function honk() {
+    document.getElementById("horn-sound").volume = 0.5;
+    document.getElementById("horn-sound").play();
+}
