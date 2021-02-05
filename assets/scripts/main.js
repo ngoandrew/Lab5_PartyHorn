711704b786a82cf02caedde06bef843b1239437c
ngoandrew <53445172+ngoandrew@users.noreply.github.com>
2021-02-05 14:37:04 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,13 +14,13 @@ document.getElementById("radio-party-horn").addEventListener("click", changeSrc)
 function changeSrc() {
     if (document.getElementById("radio-air-horn").checked) {
       document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
-      document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+      //document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
     } else if (document.getElementById("radio-car-horn").checked) { 
       document.getElementById("sound-image").src = "./assets/media/images/car.svg";
-      document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+      //document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
     } else if (document.getElementById("radio-party-horn").checked) { 
       document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
-      document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+      //document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
     }
 } 
 
